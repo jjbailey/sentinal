34cf0d1c1632b5d3e04e0d7f401831339fe52cd1
JJB <jjb@sandy.bcc.com>
2022-02-04 13:43:24 -0800

    cleanup

--- threadname.c
+++ threadname.c
@@ -2,7 +2,7 @@
  * threadname.c
  * Name the thread for monitoring with shell utils.
  *
- * Copyright (c) 2021 jjb
+ * Copyright (c) 2021, 2022 jjb
  * All rights reserved.
  *
  * This source code is licensed under the MIT license found
@@ -12,11 +12,11 @@
 #include <stdio.h>
 #include "sentinal.h"
 
-char   *threadname(char *section, char *func, char *nbuf)
+char   *threadname(char *section, char *tname, char *nbuf)
 {
 	/* strict format: <section name>_<thread name> */
 
-	snprintf(nbuf, TASK_COMM_LEN, "%.11s_%.3s", section, func);
+	snprintf(nbuf, TASK_COMM_LEN, "%.11s_%.3s", section, tname);
 	return (nbuf);
 }
 
