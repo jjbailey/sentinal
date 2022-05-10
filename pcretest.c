9a77f7b515852bcc3da486781d880b92c2e7ef76
Jack Bailey <jack@internetguy.net>
2022-05-10 14:42:03 -0700

    update pcre examples

--- pcretest.c
+++ pcretest.c
@@ -11,15 +11,26 @@
  * examples of usage:
  *
  * ends with a digit
- * $ ./pcretest '/var/log/sys.*\d$' /var/log/sys*
+ * $ pcretest '/var/log/sys.*\d$' /var/log/sys*
  * no match: /var/log/syslog
  * match:    /var/log/syslog.1
  * no match: /var/log/syslog.2.gz
-
- * negative lookbehind
- * $ ./pcretest '.*(?<!.pid)$' notapidfile pidfile.pid
+ *
+ * negative lookahead for pid files
+ * $ pcretest '(?!.*\.(?:pid)$)' notapidfile pidfile.pid
  * match:    notapidfile
  * no match: pidfile.pid
+ *
+ * negative lookbehind for pid files
+ * $ pcretest '.*(?<!\.pid)$' notapidfile pidfile.pid
+ * match:    notapidfile
+ * no match: pidfile.pid
+ *
+ * negative lookahead for compressed files
+ * $ find testdir -type f | xargs pcretest '(?!.*\.(?:bz2|gz|lz|zip|zstd)$)'
+ * no match: testdir/ddrescue-1.25.tar.lz
+ * match:    testdir/nxserver.log
+ * no match: testdir/syslog.2.gz
  */
 
 #include <stdio.h>
