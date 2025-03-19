38dae101fad2e14b101bb0e4c9a8838bf793c13b
Jack Bailey <jack@internetguy.net>
2025-03-18 17:33:22 -0700

    rename _BASE_INLINE

--- basename.h
+++ basename.h
@@ -3,15 +3,15 @@
  * replace basename(3) with inline code
  * assumes no trailing slash
  *
- * Copyright (c) 2021-2024 jjb
+ * Copyright (c) 2021-2025 jjb
  * All rights reserved.
  *
  * This source code is licensed under the MIT license found
  * in the root directory of this source tree.
  */
 
-#ifndef _BASE_INLINE
-# define	_BASE_INLINE
+#ifndef BASE_INLINE
+# define	BASE_INLINE
 
 # include <string.h>
 
@@ -21,6 +21,6 @@ static inline char *base(char *s)
 	return (p ? p + 1 : s);
 }
 
-#endif												/* _BASE_INLINE */
+#endif												/* BASE_INLINE */
 
 /* vim: set tabstop=4 shiftwidth=4 noexpandtab: */
