e10858c788a7ec02a406f3cf5735e694f44df23b
Jack Bailey <jack@internetguy.net>
2026-05-27 08:52:44 -0700

    print rm errors only when not terse

--- rmfile.c
+++ rmfile.c
@@ -2,7 +2,7 @@
  * rmfile.c
  * Remove a file or an empty directory.
  *
- * Copyright (c) 2021-2024 jjb
+ * Copyright (c) 2021-2026 jjb
  * All rights reserved.
  *
  * This source code is licensed under the MIT license found
@@ -21,11 +21,14 @@ bool rmfile(struct thread_info *ti, const char *obj, const char *remark)
 	extern bool dryrun;
 
 	if(!dryrun && remove(obj) != 0) {
-		if(strerror_r(errno, errbuf, sizeof(errbuf)) == 0) {
-			fprintf(stderr, "%s: error %s %s: %s\n", ti->ti_section, remark, obj, errbuf);
-			sleep(1);
-			return (false);
-		}
+		if(!ti->ti_terse)
+			if(strerror_r(errno, errbuf, sizeof(errbuf)) != 0) {
+				snprintf(errbuf, sizeof(errbuf), "errno %d", errno);
+				fprintf(stderr, "%s: error %s %s: %s\n", ti->ti_section, remark, obj, errbuf);
+			}
+
+		sleep(1);
+		return (false);
 	}
 
 	if(!ti->ti_terse)
